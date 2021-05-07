8fba0408a76aa4330b6569762bb9d7559f53b457
Everton Morais <esdmorais@gmail.com>
2021-05-06 23:33:37 -0400

    [WIP] Consuming external API.

--- todolist-portal/src/App.tsx
+++ todolist-portal/src/App.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>TODO List</h1>
-    </div>
-  );
+type Repo = { name: string};
+type MyState = { isLoading: boolean, repos: Repo[] };
+
+class App extends React.Component<{}, MyState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      repos: []
+    };
+
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    const url = 'https://api.github.com/users/hacktivist123/repos';
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {
+      this.setState({isLoading: true, repos: [result]})
+      result.map(a => console.log(a))
+      
+    }
+      )
+
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.fetchData}>Click me</button>
+        <ul>
+          {
+            this.state.repos.map((name, index) => (<h1>{name}</h1>))
+          }
+        </ul>
+      </div>
+    )
+  };
 }
 
 export default App;
