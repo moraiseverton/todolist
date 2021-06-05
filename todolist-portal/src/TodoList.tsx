0521019c4c28ab41727c0e9d411f11d4efba8ca4
Everton Morais <esdmorais@gmail.com>
2021-06-04 23:17:17 -0400

    Adding material-ui and creating a nice layout for every card.

--- todolist-portal/src/TodoList.tsx
+++ todolist-portal/src/TodoList.tsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import { TodoListItem } from './TodoListItem';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Grid, Paper } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 interface Props {
   cards: Card[];
   toggleCard: ToggleCard;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(1),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
+
 export const TodoList: React.FC<Props> = ({ cards, toggleCard }) => {
+  const classes = useStyles();
+
   return (
     <>
-<Container>
-  <Row>
-    <Col>1 of 2</Col>
-    <Col>2 of 2</Col>
-  </Row>
-  <Row>
-    <Col>1 of 3</Col>
-    <Col>2 of 3</Col>
-    <Col>3 of 3</Col>
-  </Row>
-</Container>
-      <Container fluid>
-        <Row className="show-grid">
-          {cards.map(card => (
-            <Col xs={4} md={3}>
-              <TodoListItem key={card.cardId} card={card} toggleCard={toggleCard} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
+        <Grid container direction="row"
+              justify="center"
+              alignItems="center"
+              spacing={1}>
+          <Grid container item xs={12} spacing={3}>
+            {cards.map(card => (
+              <Grid item xs={4}>
+                <Paper variant="outlined" square className={classes.paper}>
+                  <TodoListItem key={card.cardId} card={card} toggleCard={toggleCard} />
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
     </>
   );
 };
